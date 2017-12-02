bf0f5fa75f8d482f64f7571b8b4456aa5b618f7b
kaycee84 <34105673+kaycee84@users.noreply.github.com>
2017-12-02 15:15:02 -0500

    Update budget application
    Added some notes and made some minor changes.

--- team_awesome_budget.py
+++ team_awesome_budget.py
@@ -2,6 +2,8 @@ import csv
 
 
 def balance():
+    #This opens the transactions.csv file, load everything into a list, and compare the debits and credits.
+    #In the long run, this will be replaced by a database or maybe just move the file to the server...
     with open('transactions.csv','r') as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
 
@@ -22,6 +24,9 @@ def balance():
     print(f"You have spent ${debits:.2f} of your ${balance:.2f} deposits. You have ${balance - debits:.2f} left in your account")
 
 def update_transactions():
+    #This function updates transactions.csv to keep a running tally of our debits and credits. 
+    #Since this is a command line application at the moment, it's just a single string. 
+    #Long term, I will use fields in a web interface and just commit... I hope.
     print('Enter transaction you\'d like to add\nExample "Credit,11/11/2017,1200.00,Kevin,Metro,Wedding Deposit"')
     update = input('-> ').split(',')
     with open('transactions.csv','a',newline='') as csvfile:
@@ -29,6 +34,7 @@ def update_transactions():
         write_csv.writerow(update)
 
 def load_budget():
+    #I have another file that we use to keep note of our budget for each expenditure. This function loads that into a list
     with open('budget.csv','r') as budget_csv:
         read_csv = csv.reader(budget_csv, delimiter=',')
 
@@ -38,18 +44,24 @@ def load_budget():
             budget.append(row)
 
 def budget_check():
+    #This will take user input to get one of the categories and compare the transactions against the budget for the last
+    #30, 90, 180, and 365 days. 
+    #Once I get it working, I'll get feedback from my wife and see what features she wants added.
     pass
 
 while True:
     load_budget()
+    #bracketted words are the input options
     selection = input("Select One:\n[Check] Balance\n[Update] Transactions\nCheck [Budget]\n[Exit]\n-> ")
 
-    if selection.lower() == 'check':
+    if selection.lower(0) == 'check':
         balance()
-    elif selection.lower() == 'update':
+    elif selection.lower(0) == 'update':
         update_transactions()
-    elif selection.lower() == 'budget':
+    elif selection.lower(0) == 'budget':
         pass
-    else:
+    elif selection.lower(0) == 'exit':
         print("Goodbye.")
         break
+    else:
+        print("You must enter a valid option")
